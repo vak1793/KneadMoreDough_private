b6f2b1526477dcf26b47213c8b8aa695cf47f1ab
Arjunkrishna <arjunkrishna.venkatesan@cerner.com>
2018-06-28 23:55:57 +0530

    Added edge wrapping

--- LastSliceUWP/Models/Solver.cs
+++ LastSliceUWP/Models/Solver.cs
@@ -125,7 +125,7 @@ namespace LastSliceUWP.Models
         {
             visited[i][j] = true;
             str = str + boggle[i][j];
-            
+
             if (isValidString(str) && !toppings.Contains(str))
             {
                 Word word = new Word();
@@ -141,28 +141,156 @@ namespace LastSliceUWP.Models
             }
 
             // Traverse 8 adjacent cells of boggle[i][j]
-            for (int row = i - 1; row <= i + 1 && row < M; row++)
-                for (int col = j - 1; col <= j + 1 && col < N; col++)
-                    if (row >= 0 && col >= 0 && !visited[row][col] && isValidSubstring(str + boggle[row][col]))
-                        {
+            List<Dictionary<string, Dictionary<int, int>>> adjacentCells = getAdjacentCells(i, j);
+
+            foreach(var cell in adjacentCells)
+            {
+                foreach(string nextDir in cell.Keys)
+                {
+                    foreach(KeyValuePair<int, int> pair in cell[nextDir])
+                    {
+                        if (!visited[pair.Key][pair.Value] && isValidSubstring(str + boggle[pair.Key][pair.Value]))
                             findWordsUtil(
-                                ref found,
-                                ref toppings,
                                 boggle,
                                 visited,
                                 M,
                                 N,
-                                row,
-                                col,
+                                pair.Key,
+                                pair.Value,
                                 str,
-                                dir + getDirection(row, col, i, j),
-                                x,
-                                y
+                                dir + nextDir
                             );
-                        }
+                    }
+                }
+            }
 
             str.Remove(str.Length - 1);
             visited[i][j] = false;
+        }
+
+        List<Dictionary<string, Dictionary<int, int>>> getAdjacentCells(int i, int j) {
+            List<Dictionary<string, Dictionary<int, int>>> cells = new List<Dictionary<string, Dictionary<int, int>>>();
+
+            for (int row = i - 1; row <= i + 1; row++)
+                for (int col = j - 1; col <= j + 1; col++)
+                {
+                    string dir = getDirection(row, col, i, j);
+                    Dictionary<int, int> cell = new Dictionary<int, int>();
+
+                    int rowToAdd = row, colToAdd = col;
+
+                    if(col == -1)
+                    {
+                        if(row >= 0 && row < 5)
+                        {
+                            // r = 5, c = 15 + row
+                            rowToAdd = 5;
+                            colToAdd = row + 15;
+                        } else if (row >= 5 && row < 10)
+                        {
+                            // r = row, c = 19
+                            rowToAdd = row;
+                            colToAdd = 19;
+                        }
+                        else if (row >= 10 && row < 15)
+                        {
+                            // r = 9, c = 29 - row
+                            rowToAdd = 9;
+                            colToAdd = 29 - row;
+                        }
+                    } else if(col == 5)
+                    {
+                        if(row >= 0 && row < 5)
+                        {
+                            // r = 5, c = 9 - row
+                            rowToAdd = 5;
+                            colToAdd = 9 - row;
+                        } else if (row >= 10 && row < 15)
+                        {
+                            // r = 9, c = row - 5
+                            rowToAdd = 9;
+                            colToAdd = row - 5;
+                        }
+                    } else if(col == 20)
+                    {
+                        if(row >= 5 && row < 10)
+                        {
+                            rowToAdd = row;
+                            colToAdd = 0;
+                        }
+                    }
+
+                    if(row == -1)
+                    {
+                        if(col >=0 && col < 5)
+                        {
+                            // r = 5, c = 14 - col
+                            rowToAdd = 5;
+                            colToAdd = 14 - col;
+                        }
+                    }
+                    else if(row == 4)
+                    {
+                        if(col >= 5 && col < 10)
+                        {
+                            //r = 9 - col, c = 4
+                            rowToAdd = 9 - col;
+                            colToAdd = 4;
+                        }
+                        else if(col >= 10 && col < 15)
+                        {
+                            //r = 0, c = 14 - col
+                            rowToAdd = 0;
+                            colToAdd = 14 - col;
+                        }
+                        else if (col >= 15 && col < 20)
+                        {
+                            //r = 15 - col, col = 0
+                            rowToAdd = 15 - col;
+                            colToAdd = 0;
+                        }
+                    }
+                    else if (row == 10)
+                    {
+                        if (col >= 5 && col < 10)
+                        {
+                            //r = col + 5, c = 4
+                            rowToAdd = col + 5;
+                            colToAdd = 4;
+                        }
+                        else if (col >= 10 && col < 15)
+                        {
+                            //r = 14, c = 14 - col
+                            rowToAdd = 14;
+                            colToAdd = 14 - col;
+                        }
+                        else if (col >= 15 && col < 20)
+                        {
+                            //r = 29 - col, col = 0
+                            rowToAdd = 29 - col;
+                            colToAdd = 0;
+                        }
+                    }
+                    else if (row == 15)
+                    {
+                        if (col >= 0 && col < 5)
+                        {
+                            //r = 0, c = col
+                            rowToAdd = 0;
+                            colToAdd = col;
+                        }
+                    }
+
+                    if(rowToAdd >=0 && rowToAdd < 15 && colToAdd >=0 && colToAdd < 20)
+                    {
+                        cell.Add(rowToAdd, colToAdd);
+                        var cellWithDir = new Dictionary<string, Dictionary<int, int>>();
+                        cellWithDir.Add(dir, cell);
+                        cells.Add(cellWithDir);
+                    }
+                }
+
+            return cells;
         }
 
         public Word[] findWords(char[][] grid, int numRows, int numCols)
